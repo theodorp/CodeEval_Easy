73d4624415798ad76fdc5905e5f9325dc9661f36
Theodor Perivolaris <theodor.perivolaris@gmail.com>
2014-08-08 21:56:01 -0400

    Added Description

--- wordToDigit.py
+++ wordToDigit.py
@@ -1,3 +1,20 @@
+
+# Having a string representation of a set of numbers you need to print this numbers.
+
+# All numbers are separated by semicolon. There are up to 20 numbers in one line. The numbers are "zero" to "nine"
+
+# INPUT SAMPLE:
+# Your program should accept as its first argument a path to a filename. Each line in this file is one test case. E.g.
+
+# zero;two;five;seven;eight;four
+# three;seven;eight;nine;two
+
+# OUTPUT SAMPLE:
+# Print numbers in the following way:
+
+# 025784
+# 37892
+
 def wordToDigit(filename):
 	f = open(filename, 'r')
 	
